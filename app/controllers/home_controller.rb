687eef3e1c86d4c0b4fc964ec867f84ea27b091a
wendy bolm <wendylbolm@WENDYs-Mac-mini.local>
2015-03-11 19:24:49 -0500

    adding footer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @current_time = Time.now.to_s(:short)
+    @current_time = Time.now.stamp_like("Feb 1st 09:00 pm")
     
     @greet_message = case Time.now.hour
     when 5..11
